4c46b4ea22ebf99c2d038e0b2493b8546eaa2adc
Question Mark <18605687962@163.com>
2024-12-07 13:23:50 +0800

    update day07.

---
+++ src/day07/part01.ts
@@ -0,0 +1,31 @@
+const input = await Deno.readTextFile("./input.in");
+
+const lines = input.split(/\r?\n/);
+
+function equations_can_be_true(
+    testValue: number[],
+    target: number,
+    idx: number,
+    sum: number
+): boolean {
+    if (sum > target) return false;
+    if (idx >= testValue.length) {
+        return sum == target;
+    }
+    return equations_can_be_true(testValue, target, idx + 1, sum + testValue[idx]) ||
+        equations_can_be_true(testValue, target, idx + 1, sum * testValue[idx]);
+}
+
+let answer = 0;
+
+for (const line of lines) {
+    const target = parseInt(line.split(':')[0]);
+    const testValue = line.split(':')[1]
+        .split(' ')
+        .filter(x => x.trim().length > 0)
+        .map(x => parseInt(x));
+    if (equations_can_be_true(testValue, target, 0, 0))
+        answer += target;
+}
+
+console.log(answer);
